7aec70e15843e6c3b219232a5c2488aa456c96af
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2024-11-05 18:58:13 +0530

    avrage

---
+++ avrage.py
@@ -0,0 +1,13 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=96
+tree5=11
+
+tree=tree1+tree2+tree3+tree4+tree5
+
+print("sum of  every tree hight is:",tree)
+
+plant=tree/5
+
+print("avrage hight of tree is:",plant)
